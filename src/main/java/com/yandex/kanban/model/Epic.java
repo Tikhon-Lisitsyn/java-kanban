ac244f0b963da10a21a8d139bfab4e12899b3591
Shiz Oid <tihonlisicyn@gmail.com>
2024-07-04 23:57:27 +0300

    Убрать TaskManager из класса Epic

--- src/main/java/com/yandex/kanban/model/Epic.java
+++ src/main/java/com/yandex/kanban/model/Epic.java
@@ -9,43 +9,22 @@ import java.util.ArrayList;
 public class Epic extends Task {
     private final ArrayList<Integer> idOfSubtasks;
     private LocalDateTime endTime;
-    private final InMemoryTaskManager taskManager;
 
     public Epic() {
         idOfSubtasks = new ArrayList<>();
-        taskManager = new InMemoryTaskManager();
     }
 
     public ArrayList<Integer> getIdOfSubtasks() {
         return idOfSubtasks;
     }
-
-    public LocalDateTime getStartTime(int epicId) {
-        LocalDateTime minStartTime=taskManager.getSubtaskById(taskManager.getEpicById(epicId).getIdOfSubtasks()
-                .getFirst()).getStartTime();
-        for (Integer id:idOfSubtasks) {
-            LocalDateTime startTime=taskManager.getSubtaskById(id).getStartTime();
-            if (startTime.isBefore(minStartTime)) {
-                minStartTime=startTime;
-            }
-        }
-        return minStartTime;
-    }
-
-    public LocalDateTime getEndTime(int epicId) {
-        LocalDateTime maxEndTime=taskManager.getSubtaskById(taskManager.getEpicById(epicId).getIdOfSubtasks()
-                .getFirst()).getStartTime();
-        for (Integer id:idOfSubtasks) {
-            LocalDateTime endTime=taskManager.getSubtaskById(id).getStartTime();
-            if (endTime.isAfter(maxEndTime)){
-                maxEndTime=endTime;
-            }
-        }
-        return maxEndTime;
+    
+    @Override
+    public LocalDateTime getEndTime() {
+        return endTime;
     }
 
     public Duration getDuration(int id) {
-        return Duration.between(getStartTime(id),getEndTime(id));
+        return Duration.between(getStartTime(),getEndTime());
     }
 
     @Override
